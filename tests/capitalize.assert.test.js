e537d64ed185067511fc7cb8da133a975867ab4b
Dmitrii Svetlenko <dima.svetlenko@yandex.ru>
2023-03-06 18:28:31 +0300

    added new file test assert

---
+++ tests/capitalize.assert.test.js
@@ -0,0 +1,12 @@
+import { strict as assert } from 'node:assert';
+import { capitalize } from '../src/capitalize.js';
+
+assert(capitalize('ff') === '');
+assert(capitalize('hello') === 'Hello');
+
+// assert.equal(capitalize(''), '');
+// // Первый параметр actual – то, что пришло
+// // Второй параметр expected – то, что ожидает тест
+// // Правильный порядок аргументов имеет большое значение при анализе ошибки
+// assert.equal(capitalize('hello'), 'Hello');
+console.log('Все тесты пройдены!');
